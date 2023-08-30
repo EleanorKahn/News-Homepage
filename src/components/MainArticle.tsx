2fbc32b950096f28a25327ce9211e6ca28e8eb33
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-08-30 16:48:34 -0700

    Main article text

---
+++ src/components/MainArticle.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const MainArticle = () => {
+    return (
+        <div className='main-article'>
+            <h2 className='article-title'>The Bright Future of Web 3.0?</h2>
+            <p className='article-description'>We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. But is it really fulfilling its promise?</p>
+
+            <button>Read more</button>
+        </div>
+    );
+};
+
+export default MainArticle;
