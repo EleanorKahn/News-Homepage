50dd978420e92c9930e8c557722de1d5b0ecf33a
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-08-31 13:51:06 -0700

    css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './css/App.css'
 import Header from './components/Header';
 import MainArticle from './components/MainArticle';
 import Sidebar from './components/Sidebar';
