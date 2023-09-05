a3a5e1c45f22cc1689e1e6ff12446604fe6a9c79
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-09-05 16:32:26 -0700

    colors, layout

--- src/components/MainArticle.tsx
+++ src/components/MainArticle.tsx
@@ -4,9 +4,13 @@ const MainArticle = () => {
     return (
         <div className='main-article'>
             <h2 className='article-title'>The Bright Future of Web 3.0?</h2>
-            <p className='article-description'>We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. But is it really fulfilling its promise?</p>
+            <div>
+                <p className='article-description'>We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. But is it really fulfilling its promise?
+                </p>
+                <button className='read-more'>READ MORE</button>
+            </div>
 
-            <button className='read-more'>Read more</button>
+            
         </div>
     );
 };
