b49a3d2ea90f73c2abbc2d765a5bba918a944662
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-10-09 12:20:56 -0700

    assets

--- src/components/MainArticle.tsx
+++ src/components/MainArticle.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 const MainArticle = () => {
     return (
         <div className='main-article'>
+            <img className='main-article-image' src='./assets/images/image-web-3-desktop.jpg' alt='a colorful block puzzle'/>
             <h2 className='article-title'>The Bright Future of Web 3.0?</h2>
             <div>
                 <p className='article-description'>We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. But is it really fulfilling its promise?
